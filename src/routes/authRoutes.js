dc750619316d811ce0d406d223ea329cbed5de2b
Nathaniyell <essien.nathan@yahoo.com>
2025-02-14 15:36:06 +0100

    add registration logic

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,12 +5,25 @@ import db from "../../db.js";
 
 const router = express.Router()
 
-router.post('/register', (req, res)=>{
+router.post('/register', (req, res) => {
+    const { username, password } = req.body
 
+    //encrypt the password
+    const hashedpassword = bcrypt.hashSync(password, 8)
+    try {
+        const insertUser = db.prepare(`INSEERT INTO users(username, password) VALUES {?, ?}`)
+        const result = insertUser.run(username, hashedpassword)
+        res.sendStatus(201)
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
 })
 
-router.post('/login', (req, res)=>{
-
+router.post('/login', (req, res) => {
+    const { username, password } = req.body
+    console.log(username, password)
+    res.sendStatus(201)
 })
 
 
