ab37f8d29ffbd366846d89af87b89e1a12de043a
Nathaniyell <essien.nathan@yahoo.com>
2025-02-14 13:57:52 +0100

    serve static files to the frontend from the backend

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-const express = require("express")
+import express from "express"
+import path, { dirname } from "path"
+import { fileURLToPath } from "url"
+
 const app = express()
 const PORT = process.env.PORT || 5007
-app.use(express.json())
+
+// Get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
 
 
-app.listen(PORT, ()=>{
+// Serve the HTML file from the public directory
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
 })
