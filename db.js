512c5a0e2b1d37b7285d737a17bc0e91948efee4
Nathaniyell <essien.nathan@yahoo.com>
2025-02-14 14:21:55 +0100

    update the db  logic

--- db.js
+++ db.js
@@ -3,19 +3,21 @@ const db = new DatabaseSync(':memory:')
 
 //Execute SQL statements from strings
 db.exec(`
-    CREATE TABLE user(
+    CREATE TABLE users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT UNIQUE,
         password TEXT
     )
     `)
 db.exec(`
-    CREATE TABLE todos
+    CREATE TABLE todos (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER,
         task TEXT,
         completed BOOLEAN DEFAULT 0,,
         FOREIGN KEY(user_id) REFERENCES users(id)
+
+)
     `)
 
 //to be able to interact with this file from our backend we need to export it    
