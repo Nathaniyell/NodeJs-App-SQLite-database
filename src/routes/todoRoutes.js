301efb44fba08956432113a4b70796e731003adb
Nathaniyell <essien.nathan@yahoo.com>
2025-02-15 16:02:27 +0100

    update the todorouter to fetch all todos

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -4,23 +4,25 @@ import db from "../../db.js";
 const router = express.Router()
 
 //Get all todos for login user
-router.get('/', (req, res)=>{
-
+router.get('/', (req, res) => {
+    const getTodos = db.prepare('SELECT * FROM users WHERE user_id = ?')
+    const todos = getTodos.all(req.userId)
+    res.json(todos)
 })
 
 //Create a new todo
-router.post('/', (req, res)=>{
+router.post('/', (req, res) => {
 
 })
 
 //Update a todo
-router.put('/:id', (req, res)=>{
+router.put('/:id', (req, res) => {
 
 })
 
 
 //Delete a todo
-router.delete('/:id', (req, res)=>{
+router.delete('/:id', (req, res) => {
 
 })
 
