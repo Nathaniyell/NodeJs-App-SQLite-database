40620e7423472e6ebc5820042f21c2cf403a2965
Nathaniyell <essien.nathan@yahoo.com>
2025-02-26 21:16:54 +0100

    Fix todo retrieval query to fetch todos from correct table

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 //Get all todos for login user
 router.get('/', (req, res) => {
-    const getTodos = db.prepare('SELECT * FROM users WHERE user_id = ?')
+    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
     const todos = getTodos.all(req.userId)
     res.json(todos)
 })
