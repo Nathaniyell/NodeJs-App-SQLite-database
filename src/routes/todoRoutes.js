1dbc4fc4cffb07c727f36610327208b8e1a5ee2d
Nathaniyell <essien.nathan@yahoo.com>
2025-02-27 12:08:14 +0100

    uppdate todo creation logic

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -15,13 +15,13 @@ router.post('/', (req, res) => {
     const { task } = req.body;
     const insertTodo = db.prepare(`INSERT into todos (user_id, task) VALUES (?, ?)`)
     //the user_id specifies which user's table the task should be added to and not just any random user on the data base or all the users on the db
-    insertTodo.run(req.userId, task)
-    res.json({ id: insertTodo.lastID, task, completed: 0 })
+    const result = insertTodo.run(req.userId, task)
+    res.json({ id: result.lastInsertRowid, task, completed: 0 })
 })
 
 //Update a todo
 router.put('/:id', (req, res) => {
-
+    
 })
 
 
